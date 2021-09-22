03f5e52f48cfb4133df2a3f83d3f4518bf406c82
Jason Williamson <42687824+jasonwnc@users.noreply.github.com>
2021-09-22 12:35:15 -0400

    Update stock.py

--- stock.py
+++ stock.py
@@ -2,21 +2,32 @@ import json
 import pandas as pd
 import json
 import sqlite3
-
-
-obj = json.loads('{"AAPL": 142.94,"ORCL": 86.17,"SPY": 434.04}')
-print(obj['AAPL'])
-
+import argparse
 import requests
 
+#1 Grab a list of quotes to get form Yahoo
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-s", "--stock", dest = "stock", default="ORCL", help="Seperated by commas")
+args = parser.parse_args()
+
+print("Pulling data for:" + args.stock)
+
+stock= args.stock
+apikey="1MgUriJTmF2t2zZ4FFjpj4pvmf2H2xXLaWagZwh0"
+
+#obj = json.loads('{"AAPL": 142.94,"ORCL": 86.17,"SPY": 434.04}')
+#print(obj['AAPL'])
+
+
 url = "https://rest.yahoofinanceapi.com/v6/finance/quote"
-querystring = {"symbols":"AAPL,ORCL"}
+querystring = {"symbols":stock}
 headers = {
-    'x-api-key': "1MgUriJTmF2t2zZ4FFjpj4pvmf2H2xXLaWagZwh0"
-    }
+  'x-api-key': apikey
+   }
 response = requests.request("GET", url, headers=headers, params=querystring)
 #print(response.text)
 stock_json = response.json()
-print(stock_json['quoteResponse']['result'])
-
-
+print(stock_json['quoteResponse']['result'][0]["displayName"] + " Price:$" + str(stock_json['quoteResponse']['result'][0]["regularMarketPrice"]))
